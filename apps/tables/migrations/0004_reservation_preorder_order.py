25e6f3b4e68c2be42f9072d5bc38fc92e2733e99
neo_nurs <neocoindevelopers@gmail.com>
2026-06-24 18:03:36 +0600

    updated5

--- apps/tables/migrations/0004_reservation_preorder_order.py
+++ apps/tables/migrations/0004_reservation_preorder_order.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("orders", "0001_initial"),
+        ("orders", "0002_order_orderitem"),
         ("tables", "0003_reservation_until"),
     ]
 
